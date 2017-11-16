b6514bcfca66d2530f8a5318ceda4067d5ea5a77
Jorge <jromussi@gmail.com>
2017-11-16 02:49:37 -0300

    fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,16 @@ class ApplicationController < ActionController::Base
     'application'
     end
   end
+  layout :layout
+  
+    private
+  
+    def layout
+      # only turn it off for login pages:
+      is_a?(Devise::SessionsController) ? false : "application"
+      # or turn layout off for every devise controller:
+      !devise_controller? && "application"
+    end
 
   # LOGIN OBLICATORIO
   # def layout_by_resource
